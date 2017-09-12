240c63d349660a415759309208a21a9e4772bab6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-12 17:11:26 +0200

    [YIL-331] Jpeg native files.

---
+++ src/main/scripts/linux64/build.sh
@@ -0,0 +1,18 @@
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" -G "Unix Makefiles"
+
+make install
+r1=$?
+
+make clean
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+rm install_manifest.txt
+rm CPackConfig.cmake
+rm CPackSourceConfig.cmake
+rm jconfig.h
+
+return $r1
+
---
+++ src/main/scripts/win32/build.sh
@@ -0,0 +1,17 @@
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make install
+r1=$?
+
+make clean
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+rm install_manifest.txt
+rm CPackConfig.cmake
+rm CPackSourceConfig.cmake
+rm jconfig.h
+
+return $r1
